5c054ef2353cabbfcf856974590d74dc15ef1b15
Tautvydas <etauker@gmail.com>
2018-08-15 00:39:19 +0100

    Update 3-post-config-clone.js

--- 2-config-clone-phase/3-post-config-clone.js
+++ 2-config-clone-phase/3-post-config-clone.js
@@ -1,3 +1,9 @@
 /*
  * This script will be executed after the repository containing user configurations is cloned.
  */
+
+// Export environment variables based on configuration file.
+
+
+// Create directories defined by the user in the configuration file.
+
