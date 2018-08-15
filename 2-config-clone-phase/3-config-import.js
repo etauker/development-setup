bfa3e181c3c6cd05c8d9ce5e3890e531ff66e602
Tautvydas Kersulis <etauker@gmail.com>
2018-08-15 23:16:34 +0100

    Added selected profile import.

--- 2-config-clone-phase/3-config-import.js
+++ 2-config-clone-phase/3-config-import.js
@@ -12,6 +12,7 @@ configImportStep.run = function(oConfig) {
     let sFilepath = oConfig.workspace + "/" + sRepositoryName + "/" + oConfig.configFile;
     let sFullConfig = fs.readFileSync(sFilepath, 'utf8');
     let oFullConfig = JSON.parse(sFullConfig);
+    console.log(oFullConfig);
     oConfig.profile = sFullConfig.profiles.filter(p => p.name === oConfig.options.profileName);
     console.log(oConfig);
     return oConfig;
