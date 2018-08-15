6a32d8ddf02d47111dfbf4b341478d7312599771
Tautvydas Kersulis <etauker@gmail.com>
2018-08-16 00:18:28 +0100

    WIP

--- setup.js
+++ setup.js
@@ -76,7 +76,7 @@ let helper = require('./lib/helper.js');
         var config = oConfig.options || {};
         var options = {
             install: config.install || false,
-            profile: config.profile || "default"
+            profile: config.profile || oConfig.profile || "default"
         };
 
         // Parse command line options
